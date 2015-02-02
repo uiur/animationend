a7e78bd51a8d7dbcb0085ec6119cfdb2c37ba187
Kazato Sugimoto <uiureo@gmail.com>
2015-02-02 23:35:55 +0900

    Write tests

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+var Promise = require('es6-promise').Promise
+
 var TRANSITIONEND_EVENTS = {
   transition: 'transitionend',
   WebkitTransition: 'webkitTransitionEnd',
   MozTransition: 'mozTransitionEnd',
   OTransition: 'oTransitionEnd',
   msTransition: 'MSTransitionEnd'
-};
+}
 
 var ANIMATIONEND_EVENTS = {
   animation: 'animationend',
@@ -12,24 +14,24 @@ var ANIMATIONEND_EVENTS = {
   MozAnimation: 'mozAnimationEnd',
   OAnimation: 'oAnimationEnd',
   msAnimation: 'MSAnimationEnd'
-};
+}
 
 function animationEvents() {
-  var testElement = document.createElement('div');
+  var testElement = document.createElement('div')
 
   function availableEvents(eventMap) {
     return Object.keys(eventMap).filter(function(propertyName) {
-      return propertyName in testElement.style;
+      return propertyName in testElement.style
     }).map(function(propertyName) {
-      return eventMap[propertyName];
-    });
+      return eventMap[propertyName]
+    })
   }
 
   var events = [TRANSITIONEND_EVENTS, ANIMATIONEND_EVENTS].reduce(function(value, eventMap) {
     return value.concat(availableEvents(eventMap))
-  }, []);
+  }, [])
 
-  return events;
+  return events
 }
 
 // @param {Node} element
@@ -37,20 +39,20 @@ function animationEvents() {
 //
 // @return {Promise}
 module.exports = function animationEnd(element, callback) {
-  var events = animationEvents();
+  var events = animationEvents()
 
   return new Promise(function(resolve, reject) {
     function handler(event) {
       events.forEach(function(event) {
-        element.removeEventListener(event, handler, false);
-      });
+        element.removeEventListener(event, handler, false)
+      })
 
-      resolve(event);
-      if (callback) callback(event);
+      resolve(event)
+      if (callback) callback(event)
     }
 
     events.forEach(function(event) {
-      element.addEventListener(event, handler, false);
-    });
-  });
-};
+      element.addEventListener(event, handler, false)
+    })
+  })
+}
